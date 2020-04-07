24fbd093d269a029cbf165ff10710de133be7dd3
a.yakovlev9 <a.yakovlev9@tinkoff.ru>
2020-04-07 21:13:15 +0300

    Project initialized

---
+++ src/main/java/ru/otus/highload/socialnetwork/SocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.highload.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/highload/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.highload.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
